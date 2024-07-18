ac444d0976186d887b470eebbcd4a34dfec9c730
Mad Triathlete <h_bicakci@msn.com>
2024-07-17 21:12:41 -0400

    creation of testing framework3

---
+++ src/test/java/Utility/BrowserDriver.java
@@ -0,0 +1,19 @@
+package Utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserDriver {
+    public static WebDriver driver;
+
+    public BrowserDriver(){
+        this.driver = driver;
+        System.setProperties("webdriver.chrome.driver", System.getProperties("user.dir") + "src/test/resources/drivers/chromedriver.exe");
+        this.driver = new ChromeDriver();
+    }
+
+    public void close(){
+        this.driver.close();
+    }
+
+}
